54ce4f2160784332f8ae28069851b86ced83ffbd
Andrés Mauricio Martínez Hincapié <amartinezh@gmail.com>
2021-02-01 23:55:59 -0600

    Cambios en cabecera GNU

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@
 //
 //    You should have received a copy of the GNU General Public License
 //    along with GrowPos.  If not, see <http://www.gnu.org/licenses/>.
+
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import { DataBaseService } from './dataBaseService';
--- src/repository/mentalHealthDB/peopleDAO.ts
+++ src/repository/mentalHealthDB/peopleDAO.ts
@@ -1,3 +1,21 @@
+//    GrowPos
+//    Copyright (c) 2021 MentalHealth
+//    http://MentalHealth.co
+//
+//    This file is part of MentalHealth
+//
+//    GrowPos is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    GrowPos is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with GrowPos.  If not, see <http://www.gnu.org/licenses/>.
 
 import { DataBaseService } from '../../dataBaseService';
 import * as uuid from "uuid";
